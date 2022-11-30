964b45f4f3fda4d48e2df54d3e3e008ff75915d1
olliechaffey <olliechaff@gmail.com>
2022-11-30 20:29:04 +0000

    Active live date and time.

---
+++ scripts.js
@@ -0,0 +1,11 @@
+var datetime = new Date();
+console.log(datetime);
+document.getElementById("live-date").textContent = datetime;
+
+function refreshTime() {
+    const timeDisplay = document.getElementById("live-date");
+    const dateString = new Date().toLocaleString();
+    const formattedString = dateString.replace(", ", " - ");
+    timeDisplay.textContent = formattedString;
+  }
+    setInterval(refreshTime, 1000);
