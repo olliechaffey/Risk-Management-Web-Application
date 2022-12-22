47781769bb1bcb3b1c2e8378738260c362bf1323
olliechaffey <olliechaff@gmail.com>
2022-12-22 02:30:45 +0000

    action file updated

--- action.php
+++ action.php
@@ -7,16 +7,23 @@ $status = $_POST["status"];
 $task = $_POST["task"];
 $impact = $_POST["impact"];
 $likelihood = $_POST["likelihood"];
+$risk = $_POST["risk"];
 $risk_owner = $_POST["risk_owner"];
 
-$conn = new mysqli("localhost", "oc277_db_user", "Yevtak2d", "oc277_fyp");
 
-foreach($_POST['title'] as $key => $value){
-    $sql = 'INSERT INTO risk_assessment(title, status, task, impact, likelihood, risk_owner) VALUES ($title, $status, $task, $impact, $likelihood, $risk_owner)';
+if (!empty($_POST)) {
+    $conn = new mysqli("localhost", "oc277_ciso_user", "Yevtak2d", "oc277_finalyear");
+    if ($conn->connect_error) {
+        die("connection failed: " . $conn->connect_error);
+    }
+    
+    //$hashed = password_hash($password, PASSWORD_DEFAULT);
+    
+    $sql = "INSERT INTO risk_assessment (title, status, task, impact, likelihood, risk, risk_owner) VALUES ('$title', '$status', '$task', '$impact', '$likelihood', '$risk', '$risk_owner')";
     $result = mysqli_query($conn, $sql);
     if($result){
         mysqli_close($conn);
-        echo "Successfully added risk.";
+        echo "Data has been successfully added to the table";
     }
 }
 
